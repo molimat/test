e59db180ca6b5bd78710f2043f06290338c81bb6
molimat <matheusmolin@poli.ufrj.br>
2020-06-25 18:19:16 +0200

    Get open image dataset on

---
+++ build/darknet/x64/data/get_coco_dataset_specific_classes.py
@@ -0,0 +1,39 @@
+import os
+import json
+from os import listdir, getcwd
+from os.path import join
+
+classes = ["person","bicycle","car","motorcycle","bus"]
+
+#box form[x,y,w,h]
+def convert(size,box):
+    dw = 1./size[0]
+    dh = 1./size[1]
+    x = box[0]*dw
+    y = box[1]*dh
+    w = box[2]*dw
+    h = box[3]*dh
+    return (x,y,w,h)
+
+def convert_annotation():
+    with open('/media/yehao/ye/coco/annotations/instances_val2014.json','r') as f:
+        data = json.load(f)
+    for item in data['images']:
+        image_id = item['id']
+        file_name = item['file_name']
+        width = item['width']
+        height = item['height']
+        value = filter(lambda item1: item1['image_id'] == image_id,data['annotations'])
+        outfile = open('/media/yehao/ye/coco/labellabel/%s.txt'%(file_name[:-4]), 'a+')
+        for item2 in value:
+            category_id = item2['category_id']
+            value1 = filter(lambda item3: item3['id'] == category_id,data['categories'])
+            name = value1[0]['name']
+            class_id = classes.index(name)
+            box = item2['bbox']
+            bb = convert((width,height),box)
+            outfile.write(str(class_id)+" "+" ".join([str(a) for a in bb]) + '\n')
+        outfile.close()
+			
+if __name__ == '__main__':
+    convert_annotation()
--- scripts/get_openimages_dataset.py
+++ scripts/get_openimages_dataset.py
@@ -2,18 +2,24 @@ import csv
 import os
 
 #select classes you want to download at https://github.com/openimages/dataset/blob/master/dict.csv
-CLASS_LIST = ('/m/01g317','/m/04yx4')
+CLASS_LIST = ( '/m/01g317', #person
+                '/m/01bjv', #bus
+                '/m/0k4j', #car
+                '/m/04_sv', #motorcycle
+                '/m/07r04' #truck
+            )
 img_name = "111111111111"
+path_to_obj_folder = '../build/darknet/x64/data/obj/'
 
 #download csv from https://storage.googleapis.com/openimages/web/download.html
-with open('path\\train-annotations-bbox.csv', newline='') as csvfile:
+with open('../oidv6-train-annotations-bbox.csv', newline='') as csvfile:
     bboxs = csv.reader(csvfile, delimiter=',', quotechar='|')
     for bbox in bboxs:
         if bbox[2] in CLASS_LIST:
             if img_name != bbox[0]:
-                if not os.path.isfile("destination_path\\%s.jpg"%bbox[0]):
-                    os.system("gsutil cp gs://open-images-dataset/train/%s.jpg destination_path"%bbox[0])
-                    out_file = open("destination_path\\%s.txt"%bbox[0], 'w')
+                if not os.path.isfile(path_to_obj_folder+"%s.jpg"%bbox[0]):
+                    os.system("gsutil cp gs://open-images-dataset/train/%s.jpg %s"%(bbox[0], path_to_obj_folder))
+                    out_file = open(path_to_obj_folder+"%s.txt"%bbox[0], 'w')
                     img_name = bbox[0]
             if img_name == bbox[0]:
                 out_file.write(str(CLASS_LIST.index(bbox[2])) + " " + str(float(bbox[4])+(float(bbox[5])-float(bbox[4]))/2) + " " + str(float(bbox[6])+(float(bbox[7])-float(bbox[6]))/2)+ " " + str(float(bbox[5])-float(bbox[4])) + " " + str(float(bbox[7])-float(bbox[6])) + '\n')
